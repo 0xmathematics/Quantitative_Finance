59aa55865a37af391c66253dffdc4b764e35c690
HangMiaoEcon <miaohang@gmail.com>
2025-10-31 00:19:12 -0400

    graph_theory_visualization

---
+++ graph_theory/networkx_test.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue July 29 11:25:26 2025
+@author: Hang Miao
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import cos,sin,pi,exp,log,log2,log10,sqrt,ceil,floor
+
+#%% 
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+G.add_edge(1, 2)
+G.add_edge(1, 3)
+G.add_edge(1, 5)
+G.add_edge(2, 3)
+G.add_edge(3, 4)
+G.add_edge(4, 5)
+
+# explicitly set positions
+pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
+
+options = {
+    "font_size": 36,
+    "node_size": 3000,
+    "node_color": "white",
+    "edgecolors": "black",
+    "linewidths": 5,
+    "width": 5,
+}
+nx.draw_networkx(G, pos, **options)
+
+# Set margins for the axes so that nodes aren't clipped
+ax = plt.gca()
+ax.margins(0.20)
+plt.axis("off")
+plt.show()
+
+
+#%%
+
+
+
+
